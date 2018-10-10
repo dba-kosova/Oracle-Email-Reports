24188d7eb8dbf1b9f754714bb3fa1130d5b0ecd2
Christopher Pickering <pher122s@gmail.com>
2018-10-10 15:58:14 -0500

    added testing scripts

--- test.py
+++ test.py
@@ -1,10 +1,43 @@
-# -*- coding: utf-8 -*-
+
 
 import sys
-import os.path
-sys.path.append(os.path.join(os.path.dirname( __file__ ),'functions'))
-from my_email import Email
-from my_workbook import Workbook
+from pathlib import Path
+import basic_reports
+import time
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        print('starting ' + method.__name__)
+        try:
+            result = method(*args, **kw)
+            te = time.time()
+            print ('success running %r in %2.2f sec' % (method.__name__,  te-ts))
+
+        except Exception as e:
+            te = time.time()
+            print ('error running %r. Message %r' % (method.__name__, str(e)))
+
+    return timed
+
+# get sql prefix
+sql_report_list = set([x.stem.split('-')[0] for x in Path(__file__).parents[0].joinpath('sql').glob('*.sql') ])
+
+# get non basic reports
+non_basic_report_list = set([x.stem for x in Path(__file__).parents[0].glob('*.py') if x.stem not in ['basic_reports','test']])
+
+# basic reports
+basic_report_list = list(sql_report_list - non_basic_report_list)
 
 
-Email('test', None).SendMail()
+# test basic reports
+for report in basic_report_list:
+    if str(report) == 'test1':
+        timeit(basic_reports.main)(report)
+
+# for static reports
+for report in non_basic_report_list:
+    if str(report) == 'test2':
+
+        my_module = __import__(report, ['main']) 
+        timeit(my_module.main)()
---
+++ test2.py
@@ -0,0 +1,7 @@
+def main():
+    print("success running test2 part 1")
+
+
+
+if __name__ == "__main__":
+    main()
